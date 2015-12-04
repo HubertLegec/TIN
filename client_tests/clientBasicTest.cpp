0c9bccd1abb6c82df809a53a09e46384a526528a
HubertLegec <hubert.legec@gmail.com>
2015-12-04 23:41:25 +0100

    gtest configuration and simple test

--- client_tests/clientBasicTest.cpp
+++ client_tests/clientBasicTest.cpp
@@ -2,6 +2,12 @@
 // Created by hubert on 03.12.15.
 //
 
+#include "gtest/gtest.h"
+#include "../client/model/Model.h"
 
+TEST(ModelTest, model_get_test){
+    Model m;
+    ASSERT_EQ(5, m.getX());
+}
 
 
